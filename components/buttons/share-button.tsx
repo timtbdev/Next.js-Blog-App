3645bcb7edc595af67e409480fc3393227dfe92a
Tim Baz <tim@Tims-MacBook-Pro.local>
2023-07-28 13:50:10 -0700

    Added z level for bottom drawer for share button

--- components/buttons/share-button.tsx
+++ components/buttons/share-button.tsx
@@ -55,7 +55,7 @@ const ShareButton: React.FC<ShareButtonProps> = ({
 
   return (
     <>
-      <Drawer.Root>
+      <Drawer.Root shouldScaleBackground>
         <Drawer.Trigger asChild>
           <button
             type="button"
@@ -75,7 +75,7 @@ const ShareButton: React.FC<ShareButtonProps> = ({
         </Drawer.Trigger>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Portal>
-          <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex flex-col rounded-t-[10px] bg-zinc-100">
+          <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mt-24 flex flex-col rounded-t-[10px] bg-zinc-100">
             <div className="flex-1 rounded-t-[10px] bg-white p-4">
               <div className="mx-auto mb-8 h-1.5 w-12 flex-shrink-0 rounded-full bg-zinc-300" />
               <div className="mx-auto max-w-md">
