2df6538e9eedcff569d412ca66eed70dda845c62
Tim Baz <tim@Tims-MacBook-Pro.local>
2023-09-29 15:48:04 -0700

    Changed: login menu

--- components/site/navigations/login-menu.tsx
+++ components/site/navigations/login-menu.tsx
@@ -2,7 +2,6 @@
 
 import LoginButton from "@/components/login/login-button";
 import ProfileButton from "@/components/login/profile-button";
-import { Profile } from "@/types/collection";
 import { supabase } from "@/utils/supabase-client";
 import { Session } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
@@ -14,7 +13,6 @@ export const revalidate = 0;
 const LoginMenu = () => {
   const router = useRouter();
   const [session, setSession] = useState<Session | null>(null);
-  const [avatarUrl, setAvatarUrl] = useState<string>("");
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -30,24 +28,15 @@ const LoginMenu = () => {
     return () => subscription.unsubscribe();
   }, []);
 
-  useEffect(() => {
-    async function fetchAvatar() {
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("*")
-        .match({ id: session?.user.id })
-        .single<Profile>();
-      if (data) {
-        setAvatarUrl(data.avatar_url ? data.avatar_url : "");
-      }
-    }
-    fetchAvatar();
-  }, [session]);
-
   return (
     <>
       {session ? (
-        <ProfileButton profileImageUrl={avatarUrl} />
+        <ProfileButton
+          profileImageUrl={
+            session.user.user_metadata.picture ||
+            session.user.user_metadata.avatar_url
+          }
+        />
       ) : (
         <LoginButton />
       )}
