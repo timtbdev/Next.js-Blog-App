fb460db52194290094409e73ec5b7faa66f098c8
Tim Baz <tim@Tims-MacBook-Pro.local>
2023-09-29 16:10:52 -0700

    Fix: avatar issue globally

--- components/site/navigations/login-menu.tsx
+++ components/site/navigations/login-menu.tsx
@@ -2,6 +2,7 @@
 
 import LoginButton from "@/components/login/login-button";
 import ProfileButton from "@/components/login/profile-button";
+import { Profile } from "@/types/collection";
 import { supabase } from "@/utils/supabase-client";
 import { Session } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
@@ -13,6 +14,7 @@ export const revalidate = 0;
 const LoginMenu = () => {
   const router = useRouter();
   const [session, setSession] = useState<Session | null>(null);
+  const [avatarUrl, setAvatarUrl] = useState<string>("");
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -28,15 +30,24 @@ const LoginMenu = () => {
     return () => subscription.unsubscribe();
   }, []);
 
+  useEffect(() => {
+    async function fetchAvatar() {
+      const { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .match({ id: session?.user.id })
+        .single<Profile>();
+      if (data) {
+        setAvatarUrl(data.avatar_url ? data.avatar_url : "");
+      }
+    }
+    fetchAvatar();
+  }, [session]);
+
   return (
     <>
       {session ? (
-        <ProfileButton
-          profileImageUrl={
-            session.user.user_metadata.picture ||
-            session.user.user_metadata.avatar_url
-          }
-        />
+        <ProfileButton profileImageUrl={avatarUrl} />
       ) : (
         <LoginButton />
       )}
