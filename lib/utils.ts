6778798bc81a4130c152c1109520d1671bfdc41a
Tim Baz <tim@Tims-MacBook-Pro.local>
2023-10-05 11:48:44 -0700

    Fix: Image loading issue with dark gradient color

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,12 +52,12 @@ export const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
+      <stop stop-color="#d1d5db" offset="20%" />
+      <stop stop-color="#d7dade" offset="50%" />
+      <stop stop-color="#d1d5db" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
+  <rect width="${w}" height="${h}" fill="#d1d5db" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
