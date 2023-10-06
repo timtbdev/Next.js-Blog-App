fd5749004a32388fb1b44dc40a3f7531d8139988
Tim Baz <tim@Tims-MacBook-Pro.local>
2023-10-06 09:16:22 -0700

    Added: Progress bar for reading on post detail page

--- components/post/post-detail-header.tsx
+++ components/post/post-detail-header.tsx
@@ -1,28 +1,39 @@
-import BackButton from '@/components/buttons/back-button';
-import LoginMenu from '@/components/site/navigations/login-menu';
+"use client";
+
+import BackButton from "@/components/buttons/back-button";
+import LoginMenu from "@/components/site/navigations/login-menu";
+import { useReadingProgress } from "@/hooks/use-reading-progress";
 
 interface DetailHeaderProps {
-    title: string;
+  title: string;
 }
 
 const DetailHeader: React.FC<DetailHeaderProps> = ({ title }) => {
-    return (
-        <header className="border-y-1 sticky top-0 z-40 border-black/5 bg-gray-50/60 shadow-sm shadow-gray-300 backdrop-blur-lg">
-            <nav className="mx-auto flex max-w-5xl items-center justify-between px-6 py-4" aria-label="Global">
-                <div className="flex flex-1 items-center justify-start">
-                    <BackButton />
-                </div>
-                <div className="flex w-full max-w-3xl">
-                    <h1 className="text-md justify-start px-4 font-semibold tracking-tight text-slate-900 sm:px-0 sm:text-xl">
-                        {title}
-                    </h1>
-                </div>
-                <div className="flex flex-1 items-center justify-end">
-                    <LoginMenu />
-                </div>
-            </nav>
-        </header>
-    );
+  const completion = useReadingProgress();
+  return (
+    <header className="border-y-1 sticky top-0 z-40 border-black/5 bg-gray-50/60 shadow-sm shadow-gray-300 backdrop-blur-lg">
+      <nav
+        className="mx-auto flex max-w-5xl items-center justify-between px-6 py-4"
+        aria-label="Global"
+      >
+        <div className="flex flex-1 items-center justify-start">
+          <BackButton />
+        </div>
+        <div className="flex w-full max-w-3xl">
+          <h1 className="text-md justify-start px-4 font-semibold tracking-tight text-slate-900 sm:px-0 sm:text-xl">
+            {title}
+          </h1>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <LoginMenu />
+        </div>
+      </nav>
+      <span
+        style={{ transform: `translateX(${completion - 100}%)` }}
+        className="absolute bottom-0 h-1 w-full bg-gray-400"
+      />
+    </header>
+  );
 };
 
 export default DetailHeader;
--- supabase-image-loader.js
+++
@@ -1,6 +0,0 @@
-const projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID;
-export default function supabaseLoader({ src, width, quality }) {
-  return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&quality=${
-    quality || 75
-  }`;
-}
